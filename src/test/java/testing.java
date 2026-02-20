a8b878cfbca7d91513c74b58efb5e95ebf9eed43
SofMovUCD <Sofia.movshovich@ucdconnect.ie>
2026-02-20 18:17:55 +0000

    added testing file

---
+++ src/test/java/testing.java
@@ -0,0 +1,5 @@
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
+import static org.assertj.swing.finder.WindowFinder.findFrame;
+AssertJSwingJUnitTestCase
+public class testing {
+}
