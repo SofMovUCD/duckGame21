27e1d685f4aecd66eb3686042f72738bdbb19d3d
AMG-UCD <aidan.macgamhna@ucdconnect.ie>
2026-03-24 13:03:24 +0000

    Added bot class skeleton

---
+++ src/main/java/org/movshovich/Bot.java
@@ -0,0 +1,15 @@
+package org.movshovich;
+
+import java.awt.*;
+
+public class Bot extends Player{
+    public Bot(int playerId) {
+        super(playerId);
+    }
+
+    @Override
+    public void makeMove() {
+        //A* algorithm?
+    }
+
+}   
