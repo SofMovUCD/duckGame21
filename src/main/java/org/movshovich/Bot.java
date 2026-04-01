98fc7a1278ec5ff07e161093068af8b35740c68f
SofMovUCD <Sofia.movshovich@ucdconnect.ie>
2026-04-01 21:00:49 +0100

    bot can win when pi rule is triggered

--- src/main/java/org/movshovich/Bot.java
+++ src/main/java/org/movshovich/Bot.java
@@ -1,6 +1,5 @@
 package org.movshovich;
 
-import java.awt.*;
 import javax.swing.JButton;
 
 public class Bot extends Player{
@@ -10,6 +9,7 @@ public class Bot extends Player{
     }
 
     private int col = 10; // Our preferred vertical lane
+    private int row = 5; //used if colour changed to white (pi rule)
     private int step = 1; //distance between octagons in array (1 for black, 2 for white)
 
     @Override
@@ -17,60 +17,111 @@ public class Bot extends Player{
         //A* algorithm?
         JButton target = null;
 
-        // 1. Find our "lowest" currently placed Octagon in this column
-        int lowestRow = -1;
-        for (int r = 10; r >= 0; r--) {
-            if (Board.board[r][col][0] == this.getPlayerId()) {
-                lowestRow = r;
-                break;
-            }
-        }
-
-        // 2. If we or pi rule haven't started yet, take the top-center Octagon
-        if (lowestRow == -1) {
-            if(Board.board[0][col][0] == 0) {
-                target = Board.buttonGrid[0][col];
-            }
-            else{
-                target = findAnyAvailableOctagon();
-            }
-        }
-        else if (lowestRow < 10) {
-            // Try to go directly down (Octagon)
-            if (Board.board[lowestRow + 1][col][0] == 0) {
-                target = Board.buttonGrid[lowestRow + 1][col];
-            }
-            //Try below right rhombus (if exists)
-            else if (col < 19 && Board.board[lowestRow][col + 1][0] == getPlayerId() && Board.board[lowestRow+1][col+2][0] == 0){
-                //try to the right
-                System.out.println("checking right rhombus");
-                target = Board.buttonGrid[lowestRow + 1][col + 2];
-                col += 2;
-
-            }
-            //Try below left rhombus (if exists)
-            else if (col > 1 && Board.board[lowestRow][col - 1][0] == getPlayerId() && Board.board[lowestRow+1][col-2][0] == 0){
-                //try to the right
-                target = Board.buttonGrid[lowestRow + 1][col - 2];
-                col -= 2;
-
-            }
-            //fully blocked Try Rhombus to maneuver around
-            else {
-                // Try Rhombus Right (leads to col + 2)
-                if (col + 1 < 21 && Board.board[lowestRow][col + 1][0] == 0) {
-                    target = Board.buttonGrid[lowestRow][col + 1];
+        if(getPlayerId() == 1) { //BLACK
+            // 1. Find our "lowest" currently placed Octagon in this column
+            int lowestRow = -1;
+            for (int r = 10; r >= 0; r--) {
+                if (Board.board[r][col][0] == this.getPlayerId()) {
+                    lowestRow = r;
+                    break;
                 }
-                // Try Rhombus Left (leads to col - 2)
-                else if (col - 1 >= 0 && Board.board[lowestRow][col - 1][0] == 0) {
-                    target = Board.buttonGrid[lowestRow][col - 1];
+            }
+
+            // 2. If we or pi rule haven't started yet, take the top-center Octagon
+            if (lowestRow == -1) {
+                if (Board.board[0][col][0] == 0) {
+                    target = Board.buttonGrid[0][col];
+                } else {
+                    target = findAnyAvailableOctagonBlack();
+                }
+            } else if (lowestRow < 10) {
+                // Try to go directly down (Octagon)
+                if (Board.board[lowestRow + 1][col][0] == 0) {
+                    target = Board.buttonGrid[lowestRow + 1][col];
+                }
+                //Try below right rhombus (if exists)
+                else if (col < 19 && Board.board[lowestRow][col + 1][0] == getPlayerId() && Board.board[lowestRow + 1][col + 2][0] == 0) {
+                    //try to the right
+                    System.out.println("checking right rhombus");
+                    target = Board.buttonGrid[lowestRow + 1][col + 2];
+                    col += 2;
+
+                }
+                //Try below left rhombus (if exists)
+                else if (col > 1 && Board.board[lowestRow][col - 1][0] == getPlayerId() && Board.board[lowestRow + 1][col - 2][0] == 0) {
+                    //try to the right
+                    target = Board.buttonGrid[lowestRow + 1][col - 2];
+                    col -= 2;
+
+                }
+                //fully blocked Try Rhombus to maneuver around
+                else {
+                    // Try Rhombus Right (leads to col + 2)
+                    if (col + 1 < 21 && Board.board[lowestRow][col + 1][0] == 0) {
+                        target = Board.buttonGrid[lowestRow][col + 1];
+                    }
+                    // Try Rhombus Left (leads to col - 2)
+                    else if (col - 1 >= 0 && Board.board[lowestRow][col - 1][0] == 0) {
+                        target = Board.buttonGrid[lowestRow][col - 1];
+                    }
+                }
+            }
+        }
+        else{ //WHITE
+            // 1. Find our "rightmost" currently placed Octagon in this row
+            int RightmostColumn = -1;
+            for (int c = 20; c >= 0; c-=2) {
+                if (Board.board[row][c][0] == this.getPlayerId()) {
+                    RightmostColumn = c;
+                    break;
+                }
+            }
+
+            // 2. If we or pi rule haven't started yet, take the left-center Octagon
+            if (RightmostColumn == -1) {
+                if (Board.board[row][0][0] == 0) {
+                    System.out.println("placing white octagon in center");
+                    target = Board.buttonGrid[row][0];
+                } else {
+                    target = findAnyAvailableOctagonBlack();
+                }
+            } else if (RightmostColumn < 21) {
+                // Try to go directly right (Octagon)
+                if (Board.board[row][RightmostColumn + 2][0] == 0) {
+                    target = Board.buttonGrid[row][RightmostColumn + 2];
+                }
+                //Try below lower rhombus (if exists)
+                else if (row < 10 && Board.board[row][RightmostColumn + 1][0] == getPlayerId() && Board.board[row+1][RightmostColumn + 2][0] == 0) {
+                    //try to the right
+                    System.out.println("checking right rhombus");
+                    target = Board.buttonGrid[row+1][RightmostColumn + 2];
+                    row += 1;
+
+                }
+                //Try upper rhombus (if exists)
+                else if (row > 1 && Board.board[row - 1][RightmostColumn + 1][0] == getPlayerId() && Board.board[row - 1][RightmostColumn + 2][0] == 0) {
+                    //try to the right
+                    target = Board.buttonGrid[row - 1][RightmostColumn + 2];
+                    row -= 1;
+
+                }
+                //fully blocked Try Rhombus to maneuver around
+                else {
+                    // Try Rhombus Lower (leads to row)
+                    if (row < 20 && Board.board[row][RightmostColumn + 1][0] == 0) {
+                        target = Board.buttonGrid[row][RightmostColumn + 1];
+                    }
+                    // Try Rhombus Above (leads to row + 1)
+                    else if (col - 1 >= 0 && Board.board[row - 1][RightmostColumn + 1][0] == 0) {
+                        target = Board.buttonGrid[row - 1][RightmostColumn + 1];
+                    }
                 }
             }
         }
 
         // 5. fallback: If strategy fails, find any empty Octagon
         if (target == null /*|| target.getParent() == null*/) {
-            target = findAnyAvailableOctagon();
+            target = findAnyAvailableOctagonBlack();
         }
 
         // Execute
@@ -84,7 +135,7 @@ public class Bot extends Player{
         }*/
     }
 
-    private JButton findAnyAvailableOctagon() {
+    private JButton findAnyAvailableOctagonBlack() {
         for (int r = 0; r < BoardDraw.BOARD_Y; r++) {
             for (int c = 0; c < BoardDraw.BOARD_X; c++) {
                 if (Board.board[r][c][0] == 0 && Board.buttonGrid[r][c].getParent() != null) {
