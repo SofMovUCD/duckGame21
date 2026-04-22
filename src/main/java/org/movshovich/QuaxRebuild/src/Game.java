65d3625ac92fdf452af518c1a0410d7cea8b3e0c
SofMovUCD <Sofia.movshovich@ucdconnect.ie>
2026-04-22 10:53:12 +0100

    made piRuleBut public for testing

--- src/main/java/org/movshovich/QuaxRebuild/src/Game.java
+++ src/main/java/org/movshovich/QuaxRebuild/src/Game.java
@@ -19,7 +19,7 @@ public class Game {
     private static boolean movingFlag;
     private static boolean whiteFirst;
     private static List<Player> plrList = new ArrayList<>();
-    private static JButton piRuleBut;
+    public static JButton piRuleBut; //public just for testing
     private static JButton showStrategyBut;
     private static JButton hideStrategyBut;
     private static boolean ongoing;
